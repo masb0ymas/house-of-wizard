892b6d2bd03a0c103aace818bf8485e534000b42
masb0ymas <n.fajri@outlook.com>
2024-10-06 12:43:01 +0700

    feat: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button, Group } from "@mantine/core"
+
+export default function IndexPage() {
+  return (
+    <Group mt={250} justify="center">
+      <Button size="lg" radius="lg">Get Access</Button>
+    </Group>
+  )
+}
