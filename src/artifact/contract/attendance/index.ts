02bbb5f17fc304800b94351728ca74dc7ad5a559
masb0ymas <n.fajri@outlook.com>
2024-10-10 09:51:13 +0700

    feat: add chain base to interact contract

--- src/artifact/contract/attendance/index.ts
+++ src/artifact/contract/attendance/index.ts
@@ -1,13 +1,14 @@
-import { sepolia } from "viem/chains"
+import { base, sepolia } from "viem/chains"
 import sepolia_contract_abi from "./abi_sepolia.json"
 
 /**
  * Default Sepolia Network
- * @param chain 
- * @returns 
+ * @param chain
+ * @returns
  */
 export function getContractByChain(chain?: number) {
   const sepolia_contract_address = "0x021F46a1E13BBe4F320502D1C3887C5448CBc952"
+  const base_contract_address = "0x122e8fFB050a5771045Dbae126f65152ABc9Da28"
 
   let attendanceContract = {
     address: sepolia_contract_address,
@@ -18,6 +19,15 @@ export function getContractByChain(chain?: number) {
   if (chain === sepolia.id) {
     attendanceContract = {
       address: sepolia_contract_address,
+      abi: sepolia_contract_abi.abi,
+    } as const
+  }
+
+  // base chain
+  if (chain === base.id) {
+    attendanceContract = {
+      // @ts-expect-error
+      address: base_contract_address,
       abi: sepolia_contract_abi.abi,
     } as const
   }
