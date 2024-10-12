4e1ea57c2de850741b97ca8aebccb7e155316b86
masb0ymas <n.fajri@outlook.com>
2024-10-12 09:42:09 +0700

    fix: margin container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Welcome from "~/ui/home/Welcome"
 
 export default function page() {
   return (
-    <Container size="sm">
+    <Container size="sm" my={100}>
       <Stack align="center" gap={16}>
         <MyImage src="/static/logo-how.png" alt="logo how" width="150px" height="150px" />
         <Text component="h1" size={rem("32px")} fw={700} c={"#705C53"} my={16}>
