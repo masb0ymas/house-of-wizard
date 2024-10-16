249d111e3d721a8b5a9ac392871f98e9410eccba
masb0ymas <n.fajri@outlook.com>
2024-10-16 20:04:20 +0700

    fix: add store evm wallet signature

--- src/config/zustand.ts
+++ src/config/zustand.ts
@@ -7,13 +7,21 @@ type AuthProps = {
   access_token: string
 }
 
+type Web3WalletProps = {
+  signature: `0x${string}`
+}
+
 export type StoreProps = {
   auth: AuthProps | null
+  evm_wallet: Web3WalletProps | null
 }
 
 export type ActionProps = {
   setAuthSession: (data: AuthProps) => void
   removeAuthSession: () => void
+
+  setEvmWallet: (data: Web3WalletProps) => void
+  removeEvmWallet: () => void
 }
 
 export type StateProps = StoreProps & ActionProps
@@ -23,11 +31,17 @@ export const useStore = create<StateProps, [['zustand/persist', StateProps]]>(
   persist(
     (set) => ({
       auth: null,
+      evm_wallet: null,
 
       setAuthSession: (data: AuthProps) => {
         set((state) => ({ ...state, auth: data }))
       },
       removeAuthSession: () => set({ auth: null }),
+
+      setEvmWallet: (data: Web3WalletProps) => {
+        set((state) => ({ ...state, evm_wallet: data }))
+      },
+      removeEvmWallet: () => set({ auth: null }),
     }),
     {
       name: keyStorage, // name of the item in the storage (must be unique)
