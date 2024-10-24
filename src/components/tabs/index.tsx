19f2332aca7bed18000b30a53f374db5e2d6dd4e
masb0ymas <n.fajri@outlook.com>
2024-10-24 20:58:05 +0700

    fix: improve tabs support mobile device

--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { rem, Tabs } from '@mantine/core'
+import { useViewportSize } from '@mantine/hooks'
 import { IconHome2 } from '@tabler/icons-react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
@@ -25,18 +26,24 @@ export default function MyTabs(props: IProps) {
   const searchParams = useSearchParams()
   const tabs = searchParams.get('tabs') as string
 
+  const { width } = useViewportSize()
   const iconStyle = { width: rem(18), height: rem(18) }
+  const mobile_device = width < 780
 
   return (
     <Tabs
       keepMounted={false}
       variant="pills"
       radius="md"
-      orientation="vertical"
+      orientation={mobile_device ? 'horizontal' : 'vertical'}
       value={tabs || defaultValue}
       onChange={(value) => router.push(`${baseURL}?tabs=${value}`)}
     >
-      <Tabs.List w={150}>
+      <Tabs.List
+        w={!mobile_device ? 150 : undefined}
+        grow={mobile_device ? true : false}
+        pb={mobile_device ? 16 : undefined}
+      >
         {data.map((item) => (
           <Tabs.Tab
             key={item.key}
@@ -49,7 +56,7 @@ export default function MyTabs(props: IProps) {
       </Tabs.List>
 
       {data.map((item) => (
-        <Tabs.Panel value={item.key} pl={20} key={item.key}>
+        <Tabs.Panel value={item.key} pl={!mobile_device ? 20 : undefined} key={item.key}>
           {item.children}
         </Tabs.Panel>
       ))}
