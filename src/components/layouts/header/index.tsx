cde4ab7603ed7cfa5f1830547053d8b44906b2f6
masb0ymas <n.fajri@outlook.com>
2024-10-24 20:28:36 +0700

    fix: update navbar link active

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -3,7 +3,9 @@
 import { Anchor, Burger, Center, Container, Group, Menu, Paper } from '@mantine/core'
 import { useDisclosure, useViewportSize } from '@mantine/hooks'
 import { IconChevronDown } from '@tabler/icons-react'
+import _ from 'lodash'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import Brand from '~/components/brand'
 import classes from './partials/header.module.css'
 import { LinksGroup } from './partials/LinkGroup'
@@ -11,34 +13,56 @@ import WalletConnect from './partials/WalletConnect'
 
 const links = [
   { link: '/webinar', label: 'Webinar' },
-  { link: '/', label: 'Course' },
-  { link: '/', label: 'About' },
+  { link: '/course', label: 'Course' },
+  { link: '/about', label: 'About' },
   {
     link: '#2',
     label: 'Support',
     links: [
-      { link: '/', label: 'FAQ' },
-      { link: '/', label: 'Forums' },
-      { link: '/', label: 'Term of Condition' },
-      { link: '/', label: 'Policy & Privacy' },
+      { link: '/faq', label: 'FAQ' },
+      { link: '/forums', label: 'Forums' },
+      { link: '/term-of-condition', label: 'Term of Condition' },
+      { link: '/policy-privacy', label: 'Policy & Privacy' },
     ],
   },
 ]
 
 export default function Header() {
+  const pathname = usePathname()
+
   const { width } = useViewportSize()
   const [opened, { toggle }] = useDisclosure(false)
 
-  const desktopNavItems = links.map((link) => {
-    const menuItems = link.links?.map((item) => <Menu.Item key={item.link}>{item.label}</Menu.Item>)
+  const desktopNavItems = links.map((item) => {
+    const link_active = item.link === pathname
+    const matchPath = pathname.match(String(item.link))
+    const is_active = !_.isEmpty(matchPath) && link_active
+
+    console.log({ matchPath, is_active })
+
+    const menuItems = item.links?.map((sub_item) => {
+      const sub_link_active = item.links?.find((x) => x.link === pathname)
+      const matchPath = pathname.match(String(item.link))
+      const is_active_sub = !_.isEmpty(matchPath) && sub_link_active
+
+      return (
+        <Menu.Item
+          key={sub_item.link}
+          data-active={is_active_sub || undefined}
+          color={is_active_sub ? 'blue' : undefined}
+        >
+          {sub_item.label}
+        </Menu.Item>
+      )
+    })
 
     if (menuItems) {
       return (
-        <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
+        <Menu key={item.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
           <Menu.Target>
-            <Link href={link.link} className={classes.link}>
+            <Link href={item.link} className={classes.link}>
               <Center>
-                <span className={classes.linkLabel}>{link.label}</span>
+                <span className={classes.linkLabel}>{item.label}</span>
                 <IconChevronDown size="0.9rem" stroke={1.5} />
               </Center>
             </Link>
@@ -49,9 +73,15 @@ export default function Header() {
     }
 
     return (
-      <Link key={link.label} href={link.link} className={classes.link}>
-        {link.label}
-      </Link>
+      <Anchor
+        component={Link}
+        key={item.label}
+        href={item.link}
+        className={classes.link}
+        data-active={is_active || undefined}
+      >
+        {item.label}
+      </Anchor>
     )
   })
 
