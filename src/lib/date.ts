43c45b7604201a1e822b3692c1d66645d3eaf5b9
masb0ymas <n.fajri@outlook.com>
2024-11-06 16:32:58 +0700

    fix: add format date

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,3 +1,6 @@
+import { format, FormatOptions } from 'date-fns'
+import { id } from 'date-fns/locale'
+
 /**
  *
  * @param value
@@ -7,4 +10,16 @@ export function dateToUnixtime(value: string | Date) {
   const date = new Date(value)
   const unixTimestamp = Math.floor(date.getTime() / 1000)
   return unixTimestamp
+}
+
+/**
+ *
+ * @param value
+ * @param formatStr
+ * @param options
+ * @returns
+ */
+export function formatDate(value: string | Date, formatStr: string, options?: FormatOptions) {
+  const date = new Date(value)
+  return format(date, formatStr, { ...options, locale: id })
 }
