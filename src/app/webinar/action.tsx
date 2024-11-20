8e3c77c5743c0ac4369d1d05f6352b3a53b68d92
masb0ymas <n.fajri@outlook.com>
2024-11-20 22:12:35 +0700

    fix: query get webinars

--- src/app/webinar/action.tsx
+++ src/app/webinar/action.tsx
@@ -1,11 +1,18 @@
 'use server'
 
 import axios from 'axios'
+import qs from 'qs'
 import { env } from '~/config/env'
 import { WebinarEntity } from '~/data/entity/webinar'
 
 export async function getWebinars(): Promise<WebinarEntity[]> {
-  const res = await axios.get(`${env.API_URL}/v1/webinar?page=1&limit=12`)
+  const query = qs.stringify({
+    page: 1,
+    pageSize: 12,
+    status: 'archive',
+  })
+
+  const res = await axios.get(`${env.API_URL}/v1/webinar?${query}`)
   return res.data.data
 }
 
