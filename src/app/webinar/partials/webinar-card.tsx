92fee778160bccc056ed90fe0bb8b58d283f81f2
masb0ymas <n.fajri@outlook.com>
2024-11-21 10:38:46 +0700

    fix: check auth button join live session

--- src/app/webinar/partials/webinar-card.tsx
+++ src/app/webinar/partials/webinar-card.tsx
@@ -35,7 +35,7 @@ export default function WebinarCard(props: WebinarCardProps) {
   function renderButton() {
     if (isLive) {
       return (
-        <Link href={`/webinar/live/${slug}`}>
+        <Link href={slug}>
           <RainbowButton className="w-full h-10 rounded-xl gap-2">
             <span className="font-serif font-semibold">Join Now</span>
             <ArrowRight className="h-4 w-4 transition-transform duration-300 group-hover:translate-x-1" />
--- src/app/webinar/partials/webinar-list.tsx
+++ src/app/webinar/partials/webinar-list.tsx
@@ -1,13 +1,20 @@
 'use client'
 
+import { IconLoader } from '@tabler/icons-react'
+import { Session } from 'next-auth'
 import { useCallback, useEffect, useState } from 'react'
 import ShineBorder from '~/components/ui/shine-border'
 import { WebinarEntity } from '~/data/entity/webinar'
 import { getWebinarLiveSession, getWebinars } from '../action'
 import WebinarCard from './webinar-card'
-import { IconLoader } from '@tabler/icons-react'
 
-export default function WebinarList() {
+type IProps = {
+  session: Session | null
+}
+
+export default function WebinarList(props: IProps) {
+  const { session } = props
+
   const [webinarLive, setWebinarLive] = useState<WebinarEntity | null>(null)
   const [webinars, setWebinars] = useState<WebinarEntity[]>([])
   const [total, setTotal] = useState(0)
@@ -34,6 +41,34 @@ export default function WebinarList() {
     getLiveWebinar()
   }, [getListWebinars, getLiveWebinar])
 
+  function renderLiveButton() {
+    if (webinarLive) {
+      let baseUrl = webinarLive.slug
+
+      if (!session?.user) {
+        baseUrl = `/sign-in?callbackUrl=${encodeURIComponent(`/webinar/live/${webinarLive.slug}`)}`
+      }
+
+      return (
+        <ShineBorder
+          className="p-0 w-full h-full flex flex-col items-center justify-center overflow-hidden rounded-lg bg-background md:shadow-xl"
+          color={['#A07CFE', '#FE8FB5', '#FFBE7B']}
+        >
+          <WebinarCard
+            title={webinarLive.title}
+            slug={baseUrl}
+            description={webinarLive.description}
+            participants={webinarLive.total_participant || 0}
+            date={webinarLive.start_date}
+            isLive
+          />
+        </ShineBorder>
+      )
+    }
+
+    return null
+  }
+
   return (
     <>
       {isLoading && (
@@ -44,21 +79,7 @@ export default function WebinarList() {
       )}
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 items-center justify-center mt-8">
-        {webinarLive && (
-          <ShineBorder
-            className="p-0 w-full h-full flex flex-col items-center justify-center overflow-hidden rounded-lg bg-background md:shadow-xl"
-            color={['#A07CFE', '#FE8FB5', '#FFBE7B']}
-          >
-            <WebinarCard
-              title={webinarLive.title}
-              slug={webinarLive.slug}
-              description={webinarLive.description}
-              participants={webinarLive.total_participant || 0}
-              date={webinarLive.start_date}
-              isLive
-            />
-          </ShineBorder>
-        )}
+        {renderLiveButton()}
 
         {webinars.map((webinar) => {
           return (
