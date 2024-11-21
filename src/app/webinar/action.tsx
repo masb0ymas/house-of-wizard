7e3b475ee72580358d0f45ff4af5bedba09bd304
masb0ymas <n.fajri@outlook.com>
2024-11-21 10:21:41 +0700

    fix: improve webinar action

--- src/app/webinar/action.tsx
+++ src/app/webinar/action.tsx
@@ -1,40 +1,56 @@
 'use server'
 
 import axios from 'axios'
+import _ from 'lodash'
 import qs from 'qs'
 import { env } from '~/config/env'
 import { WebinarEntity } from '~/data/entity/webinar'
 
-export async function getWebinars(): Promise<{ result: WebinarEntity[]; total: number }> {
+type ResponseWebinars = {
+  data: WebinarEntity[]
+  total: number
+  error: string | null
+}
+
+export async function getWebinars(): Promise<ResponseWebinars> {
   const query = qs.stringify({
     page: 1,
-    pageSize: 12,
+    pageSize: 11,
     status: 'archive',
   })
 
   let result = []
   let total = 0
+  let error = null
 
   try {
     const res = await axios.get(`${env.API_URL}/v1/webinar?${query}`)
     result = res.data.data
     total = res.data.total
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    console.log(err)
+    error = _.get(err, 'response.data.message', 'Something went wrong')
   }
 
-  return { result, total }
+  return { data: result, total, error }
 }
 
-export async function getWebinarLiveSession(): Promise<WebinarEntity | null> {
+type ResponseWebinarLive = {
+  data: WebinarEntity | null
+  error: string | null
+}
+
+export async function getWebinarLiveSession(): Promise<ResponseWebinarLive> {
   let result = null
+  let error = null
 
   try {
     const res = await axios.get(`${env.API_URL}/v1/webinar/live`)
     result = res.data.data
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    console.log(err)
+    error = _.get(err, 'response.data.message', 'Something went wrong')
   }
 
-  return result
+  return { data: result, error }
 }
