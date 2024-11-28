dc2bb5695f2b36ff4f6dffec4eafb3c2797c202f
masb0ymas <n.fajri@outlook.com>
2024-11-28 21:18:26 +0700

    feat: add webinar private plan entity

---
+++ src/data/entity/webinar_private_plan.ts
@@ -0,0 +1,13 @@
+export type WebinarPrivatePlanEntity = {
+  id: string
+  created_at: string
+  updated_at: string
+  deleted_at: string | null
+  webinar_batch_id: string
+  title: string
+  description: string
+  discount: number
+  price: number
+  features: any[]
+  is_active: boolean
+}
