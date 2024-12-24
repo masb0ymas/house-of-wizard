be504304a993dfb71f037187ffba8ac5628db036
masb0ymas <n.fajri@outlook.com>
2024-12-24 20:45:20 +0700

    feat: add web3 provider

--- src/lib/provider/index.tsx
+++ src/lib/provider/index.tsx
@@ -2,19 +2,19 @@ import { SessionProvider } from 'next-auth/react'
 import { PropsWithChildren } from 'react'
 import { Toaster } from '~/components/ui/toaster'
 import WrapperNProgress from './WrapperNProgress'
-import WrapperReactQuery from './WrapperReactQuery'
+import WrapperWeb3 from './WrapperWeb3'
 
 type IProps = PropsWithChildren
 
 export default function Provider({ children }: IProps) {
   return (
     <SessionProvider>
-      <WrapperReactQuery>
+      <WrapperWeb3>
         <WrapperNProgress>
           {children}
           <Toaster />
         </WrapperNProgress>
-      </WrapperReactQuery>
+      </WrapperWeb3>
     </SessionProvider>
   )
 }
