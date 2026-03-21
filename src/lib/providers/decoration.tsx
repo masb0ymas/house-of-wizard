2bf2eb0c8e219823eb62126a723014f92dc5c6f2
masb0ymas <n.fajri@outlook.com>
2026-03-21 17:35:29 +0700

    fix: update lib decoration provider

--- src/lib/providers/decoration.tsx
+++ src/lib/providers/decoration.tsx
@@ -1,27 +1,6 @@
-import { TanStackDevtools } from '@tanstack/react-devtools'
-import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { NuqsAdapter } from 'nuqs/adapters/tanstack-router'
 import type { PropsWithChildren } from 'react'
 
-import TanStackQueryProvider from '~/integrations/tanstack-query/root-provider'
-
 export default function DecorationProvider({ children }: PropsWithChildren) {
-  return (
-    <TanStackQueryProvider>
-      {children}
-      <TanStackDevtools
-        config={{ position: 'bottom-right' }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          {
-            name: 'Tanstack Query',
-            render: <ReactQueryDevtoolsPanel />,
-          },
-        ]}
-      />
-    </TanStackQueryProvider>
-  )
+  return <NuqsAdapter>{children}</NuqsAdapter>
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,12 @@
+import { TanStackDevtools } from '@tanstack/react-devtools'
 import type { QueryClient } from '@tanstack/react-query'
-import { createRootRouteWithContext, HeadContent, Scripts } from '@tanstack/react-router'
+import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools'
+import {
+  createRootRouteWithContext,
+  HeadContent,
+  Scripts,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 
 import { GOOGLE_FONTS, META_ICONS, META_TAGS } from '~/lib/constants/meta'
 import DecorationProvider from '~/lib/providers/decoration'
@@ -33,6 +40,21 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body className="antialiased">
         <DecorationProvider>{children}</DecorationProvider>
+
+        <TanStackDevtools
+          config={{ position: 'bottom-right' }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            {
+              name: 'Tanstack Query',
+              render: <ReactQueryDevtoolsPanel />,
+            },
+          ]}
+        />
+
         <Scripts />
       </body>
     </html>
